815eff79d376cbcb8c5c82e190cb112853f9c67c
Aditya <malothaditya006@gmail.com>
2025-11-23 21:41:15 +0530

    ui: create route for discover, create-blog and app component

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DiscoverComponent } from '../discover/discover.component';
+import { AppComponent } from './app.component';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: AppComponent },
+    { path: 'discover', component: DiscoverComponent },
+    { path: 'create-blog', component: CreateBlogComponent }
+];
