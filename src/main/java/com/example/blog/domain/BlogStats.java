11ab7203f18065580d600a0678e8ddc42e994583
Aditya <malothaditya006@gmail.com>
2025-12-06 16:05:29 +0530

    backend: update the BlogStats.java

--- src/main/java/com/example/blog/domain/BlogStats.java
+++ src/main/java/com/example/blog/domain/BlogStats.java
@@ -1,13 +1,39 @@
 package com.example.blog.domain;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
 public class BlogStats {
-    private long views;
-    private long likes;
-    private long dislikes;
-    private long shares;
-    private long bookmarks;
+    @Id
+    @Schema(description = "Unique identifier for the blog")
+    private UUID blogId;
 
-    private List<User> viewers;
+    @Schema(description = "Number of likes for the blog")
+    private Integer likes;
+
+    @Schema(description = "Number of dislikes for the blog")
+    private Integer dislikes;
+
+    @Schema(description = "Number of shares for the blog")
+    private Integer shares;
+
+    @Schema(description = "Number of bookmarks for the blog")
+    private Integer bookmarks;
+
+    @Schema(description = "Users who liked the blog")
+    private Set<String> likedBy;
+
+    @Schema(description = "Users who disliked the blog")
+    private Set<String> dislikedBy;
+
+    @Schema(description = "Users who bookmarked the blog")
+    private Set<String> bookmarkedBy;
+
 }
--- src/main/java/com/example/blog/domain/User.java
+++ src/main/java/com/example/blog/domain/User.java
@@ -1,18 +1,33 @@
 package com.example.blog.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     @Column(unique = true)
+    @Schema(description = "Username of the user")
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Schema(description = "Password of the user")
+    private String password;
+
+    @Schema(description = "Firstname of the user")
     private String firstname;
+
+    @Schema(description = "Lastname of the user")
     private String lastname;
+
+    @Schema(description = "Description of the user")
     private String description;
 
-//    private UserStats stats;
+    @ElementCollection
+    @Schema(description = "Blogs created by the user")
+    private Set<String> blogs;
 }
