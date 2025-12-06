66ac2b99685e66b38e5c002a14c5d67fc3c57429
Aditya <malothaditya006@gmail.com>
2025-12-06 16:06:45 +0530

    backend: add methods of findByUsername, findByEmail in the UserRepository.java

--- src/main/java/com/example/blog/repository/UserRepository.java
+++ src/main/java/com/example/blog/repository/UserRepository.java
@@ -3,12 +3,9 @@ package com.example.blog.repository;
 import com.example.blog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
-    // Custom query methods can be defined here if needed
-    // For example, to find a user by username:
-    // Optional<User> findByUsername(String username);
+    User findByUsername(String username);
 
-    // Or to find users by role:
-    // List<User> findByRole(Role role);
+    User findByEmail(String email);
 }
